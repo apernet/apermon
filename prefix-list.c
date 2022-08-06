d1ef2198d79615730596b55a74b6cbb15cfc66ba
Honghao Zeng <nat@nat.moe>
2022-08-05 18:53:44 -0700

    preflix-list: impl

--- prefix-list.c
+++ prefix-list.c
@@ -1,9 +1,13 @@
+#include <string.h>
+#include <stdlib.h>
 #include "sflow.h"
 #include "prefix-list.h"
+#include "log.h"
 
 int apermon_prefix_match_inet(const apermon_prefix_list* lst, uint32_t addr) {
     while (lst != NULL) {
         if (lst->af != SFLOW_AF_INET) {
+            lst = lst->next;
             continue;
         }
 
@@ -21,4 +25,28 @@ int apermon_prefix_match_inet6(const apermon_prefix_list* lst, const uint8_t *ad
     // todo
 
     return 0;
+}
+
+apermon_prefix_list *new_prefix() {
+    apermon_prefix_list *list = (apermon_prefix_list *) malloc(sizeof(apermon_prefix_list));
+    memset(list, 0, sizeof(apermon_prefix_list));
+
+    return list;
+}
+
+void free_prefix_list(apermon_prefix_list *list) {
+    apermon_prefix_list *ptr = list, *prev = NULL;
+
+    while (ptr != NULL) {
+        if (prev != NULL) {
+            free(prev);
+        }
+
+        prev = ptr;
+        ptr = ptr->next;
+    }
+
+    if (prev != NULL) {
+        free(prev);
+    }
 }
--- prefix-list.h
+++ prefix-list.h
@@ -14,9 +14,13 @@ typedef struct _apermon_prefix_list {
         uint8_t mask6[16];
     };
 
-    const struct _apermon_prefix_list *next;
+    struct _apermon_prefix_list *next;
 } apermon_prefix_list;
 
 int apermon_prefix_match_inet(const apermon_prefix_list* lst, uint32_t addr);
 int apermon_prefix_match_inet6(const apermon_prefix_list* lst, const uint8_t *addr);
+
+apermon_prefix_list *new_prefix();
+void free_prefix_list(apermon_prefix_list *list);
+
 #endif // APERMON_PREFIX_LSIT_H
