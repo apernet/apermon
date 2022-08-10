e6c0ef49672fb218363dada119a47cf672f14b10
Honghao Zeng <nat@nat.moe>
2022-08-09 21:48:07 -0700

    ctx: gc

--- context.c
+++ context.c
@@ -1,6 +1,7 @@
 #include <string.h>
 #include <stdlib.h>
 #include "context.h"
+#include "log.h"
 
 apermon_context *new_context() {
     apermon_context *ctx = (apermon_context *) malloc(sizeof(apermon_context));
@@ -20,4 +21,26 @@ void free_context(apermon_context *ctx) {
     }
 
     free(ctx);
+}
+
+void gc_context(apermon_context *ctx) {
+    ctx->last_gc = ctx->now;
+    apermon_hash_item *aggr;
+    apermon_aggregated_flow *af;
+
+    aggr = ctx->aggr_hash->head;
+
+    while (aggr != NULL) {
+        af = (apermon_aggregated_flow *) aggr->value;
+        
+        if (ctx->now - af->last_modified > CONTEXT_GC_STALE_TIME) {
+            if (af->dirty) {
+                log_warn("flow staled but still dirty - something has gone wrong.\n");
+            }
+
+            aggr = hash_erase(ctx->aggr_hash, aggr, free_aflow);
+        } else {
+            aggr = aggr->next;
+        }
+    }
 }
--- context.h
+++ context.h
@@ -4,6 +4,10 @@
 #include "hash.h"
 #include "flow.h"
 #include "config.h"
+#include "time.h"
+
+#define CONTEXT_GC_MIN_INTERVAL 10 // wait at least 10s before each gc
+#define CONTEXT_GC_STALE_TIME 30 // entries not updated in 30s are removed
 
 typedef struct _apermon_cond_selected_flows apermon_cond_selected_flows;
 
@@ -16,16 +20,18 @@ typedef struct _apermon_context {
     // info about current batch of flow records
     const apermon_flows *current_flows; /* not own by us */
     const apermon_flow_record *selected_flows[MAX_RECORDS_PER_FLOW]; /* not own by us */
-    
     uint8_t flow_directions[MAX_RECORDS_PER_FLOW];
     size_t n_selected;
 
     // context (persistent) info
     apermon_hash *aggr_hash; /* hashmap: inet/inet6 to aggr, own by us */
     apermon_config_triggers *trigger_config; /* not own by us */
+    time_t now;
+    time_t last_gc;
 } apermon_context;
 
 apermon_context *new_context();
 void free_context(apermon_context *ctx);
+void gc_context(apermon_context *ctx);
 
 #endif // APERMON_CONTEXT_H
