980266aca1bd23cb1f8b8fddb4e2147a7b41dff1
Honghao Zeng <nat@nat.moe>
2022-08-10 16:07:20 -0700

    trigger: unban script

--- trigger.c
+++ trigger.c
@@ -12,7 +12,7 @@
 
 static void run_trigger_script_ban(const apermon_config_triggers *config, const apermon_config_action_scripts *script, const apermon_aggregated_flow *flow, const apermon_aggregated_flow_average *metrics) {
     char **argv = calloc(2, sizeof(char *));
-    char **envp = calloc(10, sizeof(char *));
+    char **envp = calloc(11, sizeof(char *));
     char strbuf[0xffff], addr[INET6_ADDRSTRLEN + 1], addr2[INET6_ADDRSTRLEN + 1];
 
     const apermon_config_prefix_lists_set *set = config->networks;
@@ -22,6 +22,8 @@ static void run_trigger_script_ban(const apermon_config_triggers *config, const 
     const apermon_flow_record *fr;
 
     int offset = 0, i, ret;
+
+    log_debug("running trigger script '%s' for ban event\n", script->name);
 
     stop_servers(1); // closes fds in fork
 
@@ -62,7 +64,7 @@ static void run_trigger_script_ban(const apermon_config_triggers *config, const 
 
                     snprintf(strbuf, sizeof(strbuf), "PREFIX=%s/%s", addr, addr2);
                     envp[3] = strdup(strbuf);
-                    goto end_net_and_prefix;
+                    goto ban_end_net_and_prefix;
                 }
             } else {
                 if (apermon_prefix_match_inet6(pfx, flow->inet6)) {
@@ -73,7 +75,7 @@ static void run_trigger_script_ban(const apermon_config_triggers *config, const 
                     inet_ntop(AF_INET6, pfx->mask6, addr2, sizeof(addr2));
                     snprintf(strbuf, sizeof(strbuf), "PREFIX=%s/%s", addr, addr2);
                     envp[3] = strdup(strbuf);
-                    goto end_net_and_prefix;
+                    goto ban_end_net_and_prefix;
                 }
             }
 
@@ -83,7 +85,7 @@ static void run_trigger_script_ban(const apermon_config_triggers *config, const 
         set = set->next;
     }
 
-end_net_and_prefix:
+ban_end_net_and_prefix:
     snprintf(strbuf, sizeof(strbuf), "IN_BPS=%lu", metrics->in_bps);
     envp[4] = strdup(strbuf);
 
@@ -126,12 +128,238 @@ end_net_and_prefix:
     }
 
     envp[8] = strdup(strbuf);
-    envp[9] = NULL;
+    envp[9] = strdup("TYPE=ban");
+    envp[10] = NULL;
 
     ret = execve(script->name, argv, envp);
 
     if (ret < 0) {
         log_error("execve(): %s\n", strerror(errno));
+    }
+}
+
+static void run_trigger_script_unban(const apermon_trigger_state *ts, const apermon_config_action_scripts *script) {
+    char **argv = calloc(2, sizeof(char *));
+    char **envp = calloc(8, sizeof(char *));
+    char strbuf[0xffff], addr[INET6_ADDRSTRLEN + 1], addr2[INET6_ADDRSTRLEN + 1];
+
+    const apermon_config_prefix_lists_set *set = ts->trigger->networks;
+    const apermon_config_prefix_lists *l = NULL;
+    const apermon_config_prefix_list_elements *el = NULL;
+    const apermon_prefix *pfx = NULL;
+
+    int ret;
+
+    log_debug("running trigger script '%s' for unban event\n", script->name);
+
+    stop_servers(1); // closes fds in fork
+
+    argv[0] = script->name;
+    argv[1] = NULL;
+
+    snprintf(strbuf, sizeof(strbuf), "AF=%u", ts->af);
+    envp[0] = strdup(strbuf);
+
+    if (ts->af == SFLOW_AF_INET) {
+        inet_ntop(AF_INET, &ts->inet, addr, sizeof(addr));
+    } else {
+        inet_ntop(AF_INET6, ts->inet6, addr, sizeof(addr));
+    }
+
+    snprintf(strbuf, sizeof(strbuf), "ADDR=%s", addr);
+    envp[1] = strdup(strbuf);
+
+    while (set != NULL) {
+        l = set->prefix_list;
+        el = l->elements;
+
+        while (el != NULL) {
+            pfx = el->prefix;
+
+            if (pfx->af != ts->af) {
+                el = el->next;
+                continue;
+            }
+
+            if (pfx->af == SFLOW_AF_INET) {
+                if (apermon_prefix_match_inet(pfx, ts->inet)) {
+                    snprintf(strbuf, sizeof(strbuf), "NET=%s", l->name);
+                    envp[2] = strdup(strbuf);
+
+                    inet_ntop(AF_INET, &pfx->inet, addr, sizeof(addr));
+                    inet_ntop(AF_INET, &pfx->mask, addr2, sizeof(addr2));
+
+                    snprintf(strbuf, sizeof(strbuf), "PREFIX=%s/%s", addr, addr2);
+                    envp[3] = strdup(strbuf);
+                    goto unban_end_net_and_prefix;
+                }
+            } else {
+                if (apermon_prefix_match_inet6(pfx, ts->inet6)) {
+                    snprintf(strbuf, sizeof(strbuf), "NET=%s", l->name);
+                    envp[2] = strdup(strbuf);
+
+                    inet_ntop(AF_INET6, pfx->inet6, addr, sizeof(addr));
+                    inet_ntop(AF_INET6, pfx->mask6, addr2, sizeof(addr2));
+                    snprintf(strbuf, sizeof(strbuf), "PREFIX=%s/%s", addr, addr2);
+                    envp[3] = strdup(strbuf);
+                    goto unban_end_net_and_prefix;
+                }
+            }
+
+            el = el->next;
+        }
+
+        set = set->next;
+    }
+
+unban_end_net_and_prefix:
+    snprintf(strbuf, sizeof(strbuf), "FIRST_TRIGGERED=%lu", ts->first_triggered);
+    envp[4] = strdup(strbuf);
+
+    snprintf(strbuf, sizeof(strbuf), "LAST_TRIGGERED=%lu", ts->last_triggered);
+    envp[5] = strdup(strbuf);
+
+    envp[6] = strdup("TYPE=unban");
+    envp[7] = NULL;
+
+    ret = execve(script->name, argv, envp);
+
+    if (ret < 0) {
+        log_error("execve(): %s\n", strerror(errno));
+    }
+}
+
+static void unfire_trigger(const apermon_trigger_state *ts) {
+    const apermon_config_triggers *config = ts->trigger;
+    const apermon_config_actions *action = config->action;
+    const apermon_config_action_scripts *script = NULL;
+    pid_t pid;
+
+    log_debug("trigger %s unfired\n", ts->trigger->name);
+
+    if (action == NULL) {
+        log_warn("triggered '%s' fired but no action configured\n", config->name);
+        return;
+    }
+
+    script = action->scripts;
+
+    if (script == NULL) {
+        log_warn("triggered '%s' fired but no action script configured in action '%s'\n", config->name, action->name);
+        return;
+    }
+
+    while (script != NULL) {
+        if (!(script->flags & APERMON_SCRIPT_EVENT_UNBAN)) {
+            script = script->next;
+            continue;
+        }
+
+        pid = fork();
+
+        if (pid < 0) {
+            log_error("fork(): %s\n", strerror(errno));
+        }
+
+        if (pid == 0) {
+            run_trigger_script_unban(ts, script);
+            log_error("run_trigger_script_unban returned - exiting\n");
+            exit(0);
+        }
+
+        script = script->next;
+    }
+}
+
+static void fire_trigger(const apermon_config_triggers *config, const apermon_aggregated_flow *flow, const apermon_aggregated_flow_average *metrics) {
+    apermon_trigger_state *ts = NULL, *old_ts = NULL;
+    const apermon_context *ctx = config->ctx;
+    const apermon_config_actions *action = config->action;
+    const apermon_config_action_scripts *script = NULL;
+    pid_t pid;
+
+    log_debug("trigger %s fired - in_bps %lu, out_bps %lu, in_pps %lu, out_pps %lu\n", config->name, metrics->in_bps, metrics->out_bps, metrics->in_pps, metrics->out_pps);
+
+    if (flow->flow_af == SFLOW_AF_INET) {
+        ts = hash32_find(ctx->trigger_status, &flow->inet);
+    } else {
+        ts = hash128_find(ctx->trigger_status, flow->inet6);
+    }
+
+    if (ts == NULL) {
+        ts = (apermon_trigger_state *) malloc(sizeof(apermon_trigger_state));
+        memset(ts, 0, sizeof(apermon_trigger_state));
+        ts->first_triggered = ctx->now;
+    }
+
+    ts->trigger = config;
+    ts->af = flow->flow_af;
+    ts->last_triggered = ctx->now;
+
+    if (flow->flow_af == SFLOW_AF_INET) {
+        ts->inet = flow->inet;
+        hash32_add_or_update(ctx->trigger_status, &ts->inet, ts, (void **) &old_ts);
+    } else {
+        memcpy(ts->inet6, flow->inet6, sizeof(ts->inet6));
+        hash128_add_or_update(ctx->trigger_status, ts->inet6, ts, (void **) &old_ts);
+    }
+
+    if (old_ts != ts && old_ts != NULL) {
+        log_warn("internal error: apermon_trigger_state struct replaced in hash\n");
+    }
+
+    if (old_ts != NULL) {
+        // old trigger status is not null - meaning last trigger has not been unfired yet, don't run action.
+        log_debug("trigger '%s' has not been unfired since last fired, not running action.\n", config->name);
+        return;
+    }
+
+    if (action == NULL) {
+        log_warn("triggered '%s' fired but no action configured\n", config->name);
+        return;
+    }
+
+    script = action->scripts;
+
+    if (script == NULL) {
+        log_warn("triggered '%s' fired but no action script configured in action '%s'\n", config->name, action->name);
+        return;
+    }
+
+    while (script != NULL) {
+        if (!(script->flags & APERMON_SCRIPT_EVENT_BAN)) {
+            script = script->next;
+            continue;
+        }
+
+        pid = fork();
+
+        if (pid < 0) {
+            log_error("fork(): %s\n", strerror(errno));
+        }
+
+        if (pid == 0) {
+            run_trigger_script_ban(config, script, flow, metrics);
+            log_error("run_trigger_script_ban returned - exiting\n");
+            exit(0);
+        }
+
+        script = script->next;
+    }
+}
+
+void unban_scan(apermon_context *ctx) {
+    apermon_hash_item *item = ctx->trigger_status->head;
+    apermon_trigger_state *ts = NULL;
+
+    while (item != NULL) {
+        ts = (apermon_trigger_state *) item->value;
+        if (ctx->now - ts->last_triggered > ctx->trigger_config->min_ban_time) {
+            unfire_trigger(ts);
+            item = hash_erase(ctx->trigger_status, item, free);
+        } else {
+            item = item->iter_next;
+        }
     }
 }
 
@@ -197,72 +425,10 @@ int run_trigger(const apermon_config_triggers *config, const apermon_flows *flow
         aggr = aggr->iter_next;
     }
 
+    if (ctx->now - ctx->last_unban_scan > TRIGGER_UNBAN_SCAN_INTERVAL) {
+        ctx->last_unban_scan = ctx->now;
+        unban_scan(ctx);
+    }
+
     return 0;
-}
-
-void fire_trigger(const apermon_config_triggers *config, const apermon_aggregated_flow *flow, const apermon_aggregated_flow_average *metrics) {
-    log_debug("trigger %s fired - in_bps %lu, out_bps %lu, in_pps %lu, out_pps %lu\n", config->name, metrics->in_bps, metrics->out_bps, metrics->in_pps, metrics->out_pps);
-    apermon_trigger_state *ts = NULL, *old_ts = NULL;
-    const apermon_context *ctx = config->ctx;
-    const apermon_config_actions *action = config->action;
-    const apermon_config_action_scripts *script = action->scripts;
-    pid_t pid;
-
-    if (flow->flow_af == SFLOW_AF_INET) {
-        ts = hash32_find(ctx->trigger_status, &flow->inet);
-    } else {
-        ts = hash128_find(ctx->trigger_status, flow->inet6);
-    }
-
-    if (ts == NULL) {
-        ts = (apermon_trigger_state *) malloc(sizeof(apermon_trigger_state));
-        memset(ts, 0, sizeof(apermon_trigger_state));
-    }
-
-    ts->af = flow->flow_af;
-    ts->triggered_on = ctx->now;
-
-    if (flow->flow_af == SFLOW_AF_INET) {
-        ts->inet = flow->inet;
-        hash32_add_or_update(ctx->trigger_status, &ts->inet, ts, (void **) &old_ts);
-    } else {
-        memcpy(ts->inet6, flow->inet6, sizeof(ts->inet6));
-        hash128_add_or_update(ctx->trigger_status, ts->inet6, ts, (void **) &old_ts);
-    }
-
-    if (old_ts != ts && old_ts != NULL) {
-        log_warn("internal error: apermon_trigger_state struct replaced in hash\n");
-    }
-
-    if (old_ts != NULL) {
-        // old trigger status is not null - meaning last trigger has not been unfired yet, don't run action.
-        log_debug("trigger '%s' has not been unfired since last fired, not running action.\n", config->name);
-        return;
-    }
-
-    if (action == NULL) {
-        log_warn("triggered '%s' fired but no action configured\n", config->name);
-        return;
-    }
-
-    if (script == NULL) {
-        log_warn("triggered '%s' fired but no action script configured in action '%s'\n", config->name, action->name);
-        return;
-    }
-
-    while (script != NULL) {
-        pid = fork();
-
-        if (pid < 0) {
-            log_error("fork(): %s\n", strerror(errno));
-        }
-
-        if (pid == 0) {
-            run_trigger_script_ban(config, script, flow, metrics);
-            log_error("run_trigger_script_ban returned - exiting\n");
-            exit(0);
-        }
-
-        script = script->next;
-    }
 }
--- trigger.h
+++ trigger.h
@@ -3,6 +3,7 @@
 #include "config.h"
 #include "extract.h"
 #include "flow.h"
+#define TRIGGER_UNBAN_SCAN_INTERVAL 10 // seconds
 
 typedef struct _apermon_trigger_state {
     uint8_t af; /* enum sflow_af */
@@ -12,10 +13,12 @@ typedef struct _apermon_trigger_state {
         uint8_t inet6[16];
     };
 
-    time_t triggered_on;
+    const apermon_config_triggers *trigger; // not owned by us
+
+    time_t last_triggered;
+    time_t first_triggered;
 } apermon_trigger_state;
 
 int run_trigger(const apermon_config_triggers *config, const apermon_flows *flows);
-void fire_trigger(const apermon_config_triggers *config, const apermon_aggregated_flow *flow, const apermon_aggregated_flow_average *metrics);
 
 #endif // APERMON_TRIGGER_H
