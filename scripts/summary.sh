831b4e84ba92f5d17e3f3a3403450f62c01808a5
Honghao Zeng <nat@nat.moe>
2022-08-16 19:35:10 -0700

    summary: reformat for new envs

--- scripts/summary.sh
+++ scripts/summary.sh
@@ -3,10 +3,9 @@
 TOP_FLOWS_COUNT="${TOP_FLOWS_COUNT:-10}"
 
 [ "$TYPE" = "ban" ] && {
-    printf '[a %s in "%s" is triggering ban]\n\n' "$AGGREGATOR" "$NET" 
-    printf 'by: %s\n' "$TRIGGER"
+    printf '[%s banned a %s]\n\n' "$TRIGGER" "$AGGREGATOR" 
     printf '%s: %s\n' "$AGGREGATOR" "$TARGET"
-    printf 'prefix: %s\n' "$PREFIX"
+    printf 'prefix-list: %s > %s\n' "$NET" "$PREFIX"
     printf 'in: %d Mbps, %d pps\n' "$((IN_BPS / 1000000))" "$IN_PPS"
     printf 'out: %d Mbps, %d pps\n' "$((OUT_BPS / 1000000))" "$OUT_PPS"
     printf '\n'
@@ -18,10 +17,9 @@ TOP_FLOWS_COUNT="${TOP_FLOWS_COUNT:-10}"
 }
 
 [ "$TYPE" = "unban" ] && {
-    printf '[a %s in "%s" is triggering unban]\n\n' "$AGGREGATOR" "$NET" 
-    printf 'by: %s\n' "$TRIGGER"
+    printf '[%s unbanned a %s]\n\n' "$TRIGGER" "$AGGREGATOR" 
     printf '%s: %s\n' "$AGGREGATOR" "$TARGET"
-    printf 'prefix: %s\n' "$PREFIX"
+    printf 'prefix-list: %s > %s\n' "$NET" "$PREFIX"
     printf 'peak in: %d Mbps, %d pps\n' "$((PEAK_IN_BPS / 1000000))" "$PEAK_IN_PPS"
     printf 'peak out: %d Mbps, %d pps\n' "$((PEAK_OUT_BPS / 1000000))" "$PEAK_OUT_PPS"
     printf 'first triggered: %s\n' "`date -d @$FIRST_TRIGGERED`"
